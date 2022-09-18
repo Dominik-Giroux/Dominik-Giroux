51e49313526882e2131287882c1331747eb08b49
Dominik Giroux <giroux.dominik@gmail.com>
2022-09-18 16:14:22 -0400

    small cleanup

--- components/newsletter.js
+++ components/newsletter.js
@@ -36,18 +36,18 @@ export default function Newsletter({ context }) {
           <p className="mt-3 max-w-3xl text-lg leading-6 text-zinc-100">{newsletter.description[context.locale]}</p>
         </div>
         <div className="mt-8 lg:mt-0 lg:ml-8">
-          <form className="sm:flex">
-            <label htmlFor="email-address" className="sr-only">
+          <form action="" method="post" className="sm:flex">
+            <label htmlFor="email" className="sr-only">
               {newsletter.form.email.label[context.locale]}
             </label>
             <input
-              id="email-address"
-              name="email-address"
+              id="email"
+              name="email"
               type="email"
               autoComplete="email"
-              required
               className="w-full rounded-md border border-transparent px-5 py-3 placeholder-gray-500 focus:border-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 sm:max-w-xs"
               placeholder={newsletter.form.email.placeholder[context.locale]}
+              required
             />
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
               <button
