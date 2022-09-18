62bdaa3fe8215b255b6918cc0dad1532c406a8dd
Dominik Giroux <giroux.dominik@gmail.com>
2022-09-18 13:51:08 -0400

    testing meta & img

--- components/sections/clients.js
+++ components/sections/clients.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Clients({ context }) {
+export default function Clients({ context, clients }) {
   const title = {
     en: "Blockchains - Cloud Solutions - eCommerces - Progressive Web Apps - Blogs - Websites",
     fr: "Blockchains - Solutions Cloud - Commerce en ligne - PWA - Blogues - Sites Web"
@@ -24,22 +24,16 @@ export default function Clients({ context }) {
         </h1>
         <p className="text-lg font-black text-zinc-600">{intro[context.locale]}</p>
         <div className="mx-auto grid max-w-4xl grid-cols-2 gap-4 sm:grid-cols-4">
-          <Image src="/img/clients/peertracks.png" alt="Peertracks" width={300} height={300} layout="responsive" />
-          <Image src="/img/clients/soundac.png" alt="SounDAC" width={300} height={300} layout="responsive" />
-          <Image
-            src="/img/clients/industrie-lassonde.png"
-            alt="Industrie Lassonde"
-            width={300}
-            height={300}
-            layout="responsive"
-          />
-          <Image
-            src="/img/clients/wolters-kluwer.png"
-            alt="Wolters Kluwer"
-            width={300}
-            height={300}
-            layout="responsive"
-          />
+          {clients.map((client, i) => (
+            <Image
+              key={`client-img-${i}`}
+              src={client.src}
+              alt={client.alt}
+              width={300}
+              height={300}
+              layout="responsive"
+            />
+          ))}
         </div>
         <p className="text-base font-thin text-zinc-600">{outtro[context.locale]}</p>
       </div>
